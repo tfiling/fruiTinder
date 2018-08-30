8430989658573e59f15fb1ebb421faa604d54532
Eldar <eldarlandman@gmail.com>
2018-08-30 22:37:15 +0300

    Added customer/rgister route and pass validate handling

--- bl/customer/customers.js
+++ bl/customer/customers.js
@@ -1,7 +1,25 @@
 
+ var passwordValidator = require('password-validator');
+ const customersRepo = require('../../dal/customer/customers');
+
+exports.registerCustomer = (req, res) => {   
+   var user=req.headers.user_name;
+   var pass=req.headers.password;
+   var schema= new passwordValidator();
+   schema
+    .is().min(8)                                    // Minimum length 8
+    .is().max(100)                                  // Maximum length 100
+    .has().uppercase()                              // Must have uppercase letters
+    .has().lowercase().has().not().spaces();                              // Must have lowercase letters
+
+    if (schema.validate(pass)){
+        customersRepo.addUserToDB(user,pass);
+        res.sendStatus(200);
+    }
+    else{
+        res.sendStatus(403);
+    }
+    
 
 
-exports.registerCustomer = (req, res, next) => {
-    res.sendStatus('200');
-    // res.send('200');
 }
